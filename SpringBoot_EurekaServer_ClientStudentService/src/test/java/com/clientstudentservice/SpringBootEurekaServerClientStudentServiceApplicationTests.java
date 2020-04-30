98b7b1115190e0b5516ffec5554ab8688b4771df
manish sharma <manish.sharma23@gmail.com>
2020-04-30 22:01:57 +0530

    client student service

---
+++ SpringBoot_EurekaServer_ClientStudentService/src/test/java/com/clientstudentservice/SpringBootEurekaServerClientStudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientstudentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEurekaServerClientStudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
